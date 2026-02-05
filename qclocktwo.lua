9937d82db67844517a8c7b0b13b2540c8aa51b0f
m0wmt <nobody@example.com>
2026-02-05 13:56:54 +0000

    Add comments, fix justification of minute dots

--- qclocktwo.lua
+++ qclocktwo.lua
@@ -1,5 +1,9 @@
+-- A simple function to return the minute dot to display underneath the 
+-- words of qclocktwo. Was unable to do this in conky itself as it does not
+-- have moduluo operations available that I could find. I would have had to 
+-- many many if statements in conky to achieve this!
 function conky_minute()
     --print(os.date("minute is %M"))
-    local result = math.fmod(os.date("%M"), 5)
-    return result
+    --local result = math.fmod(os.date("%M"), 5)
+    return math.fmod(os.date("%M"), 5)
 end
